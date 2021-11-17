56271d0997fcf696612a501996d2c63925f7d6f0
Kristian Klamas <kklamas@tktk.ee>
2021-11-17 14:26:21 +0200

    Week9

---
+++ ShoppingList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace ShoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //kausta nimi shopping list
+            //faili nimi myshoppinglist.txt
+
+            string rootDirectory = @"C:\Users\Kristian\Desktop\Programmeerimise algkursus\Samples";
+            Console.WriteLine("Sisesta kausta nimi 'shoppingList'");
+            string newDirectory = Console.ReadLine();
+            Console.WriteLine("Sisesta faili nimi: 'myShoppingList.txt");
+            string fileName = Console.ReadLine();
+
+
+            if (Directory.Exists($"{rootDirectory}\\{newDirectory}") && File.Exists($"{rootDirectory}\\{newDirectory}\\{fileName}"))
+            {
+                Console.WriteLine($"Directory and file exist.");
+            }
+            else
+            {
+                Directory.CreateDirectory($"{rootDirectory}\\{newDirectory}");
+                File.Create($"{rootDirectory}\\{newDirectory}\\{fileName}");
+            }
+        }
+    }
+}
---
+++ ShoppingListAdd/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ShoppingListAdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileLocation = @"C:\Users\...\Samples\shoppingList";
+            string fileName = @"\\myShoppingList.txt";
+
+            string[] arrayFromFile = File.ReadAllLines($"{fileLocation}{fileName}");
+            List<string> myWishList = arrayFromFile.ToList<string>();
+
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Kas soovid midagi ostunimekirja lisada? Kui jah, vajuta '+', kui ei, vajuta '-'");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (userInput == '+')
+                {
+                    Console.WriteLine("Sisesta oma ostusoov:");
+                    string userWish = Console.ReadLine();
+                    myWishList.Add(userWish);
+                }
+                else
+                {
+                    loopActive = false;
+                    Console.WriteLine("Ole tubli!");
+                }
+
+            }
+
+            Console.Clear();
+
+            foreach (string wish in myWishList)
+            {
+                Console.WriteLine($"Sinu ostusoov: {wish}");
+            }
+
+            File.WriteAllLines($"{fileLocation}{fileName}", myWishList);
+
+        }
+    }
+}
